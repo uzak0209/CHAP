18355d3438cff8576ae566eb519063be9ef32c0f
uzak0209 <kazukki_0209@icloud.com>
2025-05-19 09:07:03 +0900

    googleアカウント連携後 cookieeにJWT token保存

--- google/go.mod
+++ google/go.mod
@@ -4,6 +4,9 @@ go 1.23.0
 
 toolchain go1.23.9
 
-require golang.org/x/oauth2 v0.30.0
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	golang.org/x/oauth2 v0.30.0
+)
 
 require cloud.google.com/go/compute/metadata v0.3.0 // indirect
--- google/google.go
+++ google/google.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -55,9 +57,32 @@ func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println("User Info:", userInfo)
+	jwtToken := generateJWT(userInfo)
+	fmt.Fprintf(w, `
+		<!DOCTYPE html>
+		<html>
+		<head><meta charset="utf-8"><title>ログイン中...</title></head>
+		<body>
+			<script>
+				localStorage.setItem("jwt", %q);
+				window.location.href = "https://localhost:8443";
+			</script>
+			<p>ログイン中...</p>
+		</body>
+		</html>
+	`, jwtToken)
 	http.Redirect(w, r, "https://localhost:8443", http.StatusTemporaryRedirect)
 }
+func generateJWT(userInfo map[string]interface{}) string {
+	claims := jwt.MapClaims{
+		"email": userInfo["email"],
+		"exp":   time.Now().Add(24 * time.Hour).Unix(),
+	}
 
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, _ := token.SignedString([]byte("your-secret-key"))
+	return signed
+}
 func main() {
 	http.HandleFunc("/auth/google", handleGoogleLogin)
 	http.HandleFunc("/auth/google/callback", handleGoogleCallback)
