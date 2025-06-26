acfd0e4ca568eef0bba99fa93fefe6a489622bbb
uzak0209 <kazukki_0209@icloud.com>
2025-06-26 12:23:10 +0900

    .nextの削除

--- web/templates_backup/scripts/types.js
+++
@@ -1,7 +0,0 @@
-//MapObjectのタイプを定義
-export var ObjectType;
-(function (ObjectType) {
-    ObjectType["MESSAGE"] = "MESSAGE";
-    ObjectType["THREAD"] = "THREAD";
-    ObjectType["EVENT"] = "EVENT";
-})(ObjectType || (ObjectType = {}));
